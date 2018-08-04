799455e8769d3a1ad4a8d4be3442b6fd10c11ef5
Markus Spanier <spanierm@gmx.net>
2018-08-04 23:16:57 +0200

    Initial commit

---
+++ src/main/java/com/example/springhateoasclient/SpringHateoasClientApplication.java
@@ -0,0 +1,12 @@
+package com.example.springhateoasclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHateoasClientApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringHateoasClientApplication.class, args);
+  }
+}
